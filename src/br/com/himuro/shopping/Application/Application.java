4b595e332ee3073f3bcf37c109045e936875b3a7
vihimuro <vihi1208@gmail.com>
2023-10-09 07:49:52 -0400

    feat: challenge completed

--- src/br/com/himuro/shopping/Application/Application.java
+++ src/br/com/himuro/shopping/Application/Application.java
@@ -1,7 +1,41 @@
 package br.com.himuro.shopping.Application;
 
+import br.com.himuro.shopping.Models.CreditCard;
+import br.com.himuro.shopping.Models.Transaction;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the card limit: ");
+        double limit = sc.nextDouble();
+        System.out.println();
+        CreditCard creditCard = new CreditCard(limit);
+        int i = -1;
+        while (i != 0){
+            try {
+                sc.nextLine();
+                System.out.print("Enter the purchase description: ");
+                String purchaseDescription = sc.nextLine();
 
+                System.out.print("Enter the purchase value: ");
+                double purchaseValue = sc.nextDouble();
+                Transaction transaction = new Transaction(purchaseDescription, purchaseValue);
+                boolean isTransactionCompleted = creditCard.addTransaction(transaction);
+                if (isTransactionCompleted){
+                    System.out.println("Purchase completed!");
+                }
+                else {
+                    System.out.println("Insufficient Balance!");
+                }
+                System.out.println("Press 0 to exit or 1 to continue");
+                i = sc.nextInt();
+            }
+            catch (Error e) {
+                System.out.println("Invalid input");
+            }
+        }
+        creditCard.showPurchases();
     }
 }
--- src/br/com/himuro/shopping/Models/CreditCard.java
+++ src/br/com/himuro/shopping/Models/CreditCard.java
@@ -1,11 +1,18 @@
 package br.com.himuro.shopping.Models;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class CreditCard {
     private double limit;
     private double moneyExpended;
     private List<Transaction> transactions;
+
+    public CreditCard(double limit) {
+        this.limit = limit;
+        this.transactions = new ArrayList<>();
+    }
 
     public double getLimit() {
         return limit;
@@ -33,10 +40,19 @@ public class CreditCard {
             return false;
         }
         transactions.add(transaction);
-        this.moneyExpended -= transaction.getValue();
+        this.moneyExpended += transaction.getValue();
         return true;
     }
 
+    public void showPurchases(){
+        StringBuilder message = new StringBuilder("Purchases made: \n\n");
+        transactions.sort(Comparator.comparing(Transaction::getValue));
+        for ( Transaction transaction : transactions) {
+            message.append(transaction.toString()).append("\n\n");
+        }
+        message.append("Card balance: ").append(String.format("%.1f", getBalance()));
+        System.out.println(message);
+    }
 
 
 }
